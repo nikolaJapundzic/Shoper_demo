bd6564aa91959592d29389d810e1fc1dc3428113
Nikola <nikola_japundzic@outlook.com>
2020-06-25 22:54:17 -0400

    first commit

--- app/src/main/java/com/example/shoper_demo/MainActivity.java
+++ app/src/main/java/com/example/shoper_demo/MainActivity.java
@@ -15,13 +15,24 @@ import android.widget.ListView;
 import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    //String[] ninjaList1 = {"Hyannis", "Falmouth", "Orleans", "South Yarmouth", "Harwhich Port"};
+    //ArrayList<String> listItems=new ArrayList<>(Arrays.asList(ninjaList1));
 
-    String[] ninjaList1 = {"11.3", "55.21"};
+    String[] townsStringArray = {"Hyannis, Ma", "East Falmouth, Ma", "Orleans, Ma", "South Yarmouth, Ma", "Harwhich Port, Ma"};
+    String[] addresStringArray = {"Shaw's ∙ 1070 Iyannough Rd, STE 1", "Shaw's ∙ 137 Tea Ticket Highway", "Shaw's ∙ 9 West Road", "Shaw's ∙ 1108 Route 28", "Shaw's ∙ 18 Sisson Road"};
 
-    ArrayList<String> listItems=new ArrayList<>(Arrays.asList(ninjaList1));
+    ArrayList<String> listItemsViewValue=new ArrayList<>();
+    ArrayList<String> listItemsViewMiles=new ArrayList<>();
+    ArrayList<String> listItemsItemsUnits=new ArrayList<>();
+
+    //ArrayList<String> listItemsViewTownString=new ArrayList<>(Arrays.asList(townsStringArray));
+    ArrayList<String> listItemsViewTown=new ArrayList<>();
+    ArrayList<String> listItemsViewAddres=new ArrayList<>();
+    Random rand = new Random();
 
 
     @Override
@@ -41,11 +52,30 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void run() {
-                listItems.add("sadas");
+                boolean Flag = false;
+                double valueDolar = 0;
+                while(Flag == false){
+                    valueDolar = rand.nextInt(10000 - 700);
+                    valueDolar = valueDolar / 100;
+                    valueDolar = valueDolar + 7;
+                    if(String.valueOf(valueDolar).length()<=5){
+                        Flag = true;
+                    }
+                }
+                
+
+                listItemsViewValue.add(String.valueOf(valueDolar));
+                listItemsViewMiles.add((Double.valueOf(rand.nextInt(300))/10) + " miles");
+                listItemsItemsUnits.add(rand.nextInt(100)+ "\\" + rand.nextInt(100));
+
+                int pos = rand.nextInt(townsStringArray.length);
+                listItemsViewTown.add(townsStringArray[pos]);
+                listItemsViewAddres.add(addresStringArray[pos]);
+
                 customadapter1.notifyDataSetChanged();
-                handler.postDelayed( this, 60 * 100 );
+                handler.postDelayed( this, 30 * 100 );
             }
-        }, 60 * 100 );
+        }, 30 * 100 );
 
 
 
@@ -71,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return listItems.size();
+            return listItemsViewValue.size();
         }
 
         @Override
@@ -88,9 +118,17 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
             view = getLayoutInflater().inflate(R.layout.batch,null);
-            TextView textViewValue = (TextView)view.findViewById(R.id.textView2);
+            TextView textViewValue = (TextView)view.findViewById(R.id.textViewValue);
+            TextView textViewMiles = (TextView)view.findViewById(R.id.textViewMiles);
+            TextView textViewItemsUnits = (TextView)view.findViewById(R.id.textViewItemsUnits);
+            TextView textViewTown = (TextView)view.findViewById(R.id.textViewTown);
+            TextView textViewAddres = (TextView)view.findViewById(R.id.textViewAddres);
 
-            textViewValue.setText(listItems.get(i));
+            textViewValue.setText(listItemsViewValue.get(i));
+            textViewMiles.setText(listItemsViewMiles.get(i));
+            textViewItemsUnits.setText(listItemsItemsUnits.get(i));
+            textViewTown.setText(listItemsViewTown.get(i));
+            textViewAddres.setText(listItemsViewAddres.get(i));
             //This line belowe add frame around items in list view
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 view.setBackground(getDrawable(R.drawable.listview_item_border));
